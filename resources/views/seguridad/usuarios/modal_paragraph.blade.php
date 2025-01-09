d1341912741f46b7362f7db7e903c934c4151e72
hhuezo <hugo.alex.huezo@gmail.com>
2025-01-09 10:28:24 -0600

    vista roles

---
+++ resources/views/seguridad/usuarios/modal_paragraph.blade.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="modal-paragraph-{{ $obj->id }}">
+    <div class="modal-dialog modal-lg">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">{{$obj->name}}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal">
+                </button>
+            </div>
+            <div class="modal-body">
+                <h6 class="text-base text-slate-900 dark:text-white leading-6">
+                    <p class="lead mb-0">
+                        {{$obj->paragraph_section8}}
+                    </p>
+                </h6>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
