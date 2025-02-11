7e5e723698e2451d79dde8563cb2611729bb2149
jramirez007 <jerf07@yahoo.com>
2025-02-10 19:34:27 -0600

    cambiar pagina finalizar examen

--- resources/views/examen/index.blade.php
+++ resources/views/examen/index.blade.php
@@ -14,7 +14,7 @@
                 style="min-height: 50vh; padding-top: 10vh; padding-bottom: 10vh;">
                 <div class="w-70">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 24 24" fill="none"
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 24 24" fill="none"
                         stroke="#00A6BF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                         class="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed-check">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -27,14 +27,16 @@
                         <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
                         <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
                         <path d="M9 12l2 2l4 -4" />
-                    </svg>
+                    </svg> --}}
+
+                    <svg width="128px" height="128px" viewBox="0 0 48 48" version="1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 48 48" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle fill="#4CAF50" cx="24" cy="24" r="21"></circle> <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"></polygon> </g></svg>
                     <br><br>
 
                     <div class="d-flex flex-column flex-md-row justify-content-center gap-2">
                         @if (session('id') == '1')
-                            <p class="fs-2 fw-bold m-0" style="color: rgb(0, 186, 208);">Congratulations</p>
+                            <p class="fs-2 fw-bold m-0" style="color: #4CAF50;">Congratulations</p>
                         @else
-                            <p class="fs-2 fw-bold m-0" style="color: rgb(0, 186, 208);">Felicidades</p>
+                            <p class="fs-2 fw-bold m-0" style="color: #4CAF50;">Felicidades</p>
                         @endif
 
                     </div>
@@ -56,9 +58,9 @@
 
                     <div class="d-flex flex-column flex-md-row justify-content-center gap-2">
                         @if (session('id') == '1')
-                            <p class="fs-2 fw-bold m-0" style="color: rgb(0, 186, 208);"> Exam completed on</p>
+                            <p class="fs-2 fw-bold m-0" style="color: #4CAF50;"> Exam completed on</p>
                         @else
-                            <p class="fs-2 fw-bold m-0" style="color: rgb(0, 186, 208);"> Examen completado en</p>
+                            <p class="fs-2 fw-bold m-0" style="color: #4CAF50;"> Examen completado en</p>
                         @endif
 
                     </div>
@@ -97,7 +99,7 @@
 
                             <button aria-label="Iniciar" class="btn btn-outline-info text-white text-xl btn-sm"
                                 type="submit"
-                                style="border-radius: 1.5rem; background-color: #1A365E; padding: 0.5rem 2rem; font-size: 1rem; border: 0;">
+                                style="border-radius: 1.5rem; background-color: #054B99; padding: 0.5rem 2rem; font-size: 1rem; border: 0;">
                                 <strong>
                                     {{ session('id') == '1' ? 'Finish' : 'Finalizar' }}
                                 </strong>
